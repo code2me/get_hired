3365a4d3bfcb0da856eda9e9a658a67d79bd1067
Navneet Bahuguna <navneetbahuguna007@gmail.com>
2023-10-24 15:51:39 +0530

    fix: fetch single job

--- src/app/jobs/[id]/page.tsx
+++ src/app/jobs/[id]/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import React from "react";
 
 const getData = async (id: string) => {
-  const res = await fetch(`/api/jobs/${id}`, {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/jobs/${id}`, {
     cache: "no-store",
   });
 
